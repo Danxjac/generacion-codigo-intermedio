7d14b052f5f56134444d62a222fd321945370d78
Danxjac <98290517+Danxjac@users.noreply.github.com>
2022-03-07 14:00:37 -0400

    Add files via upload

---
+++ Generador de Código Intermedio.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+import re
+j=1;y=[]
+op=['+','-','*','/']
+x=input("Entra expresiones: ")
+m=re.split("([+-/*])",x)
+def comp(m,j):
+    for palabra in m:
+        if palabra in op:
+            y.append(''.join(m[0:3]))
+            m.pop(0);m.pop(0)
+            m[0]='t'+str(j)
+            j+=1
+    return m,j
+while len(m)>1:
+        m,j=comp(m,j)
+k=len(y)
+y.reverse()
+for i in range(0,len(y)):
+    print ('t'+str(k)+'='+y[i] )
+    k-=1
+            
